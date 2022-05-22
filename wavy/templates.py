10b1e41c107e0df01a3d0ead77dda8424a5b6b55
magdychserg <magdych@mail.ru>
2022-05-22 20:09:34 +1200

    lesson3

--- wavy/templates.py
+++ wavy/templates.py
@@ -1,28 +1,8 @@
-from jinja2 import Template
-import os
+from jinja2 import Environment, FileSystemLoader
 
 
 def rendering(template_name, folder='templates', **kwargs):
-    """
-    :param template_name: имя шаблона
-    :param folder: папка в которой ищем шаблон, для примера меняем путь на ../templates
-    :param kwargs: параметры
-    :return:
-    """
-    file_path = os.path.join(folder, template_name)
-    # Открываем шаблон по имени
-    with open(file_path, encoding='utf-8') as f:
-        # Читаем
-        template = Template(f.read())
-    # рендерим шаблон с параметрами
-    return template.render(**kwargs)
-
-
-if __name__ == '__main__':
-    # Пример использования
-    output_test = rendering('index.html', workers={
-        'position': 'director',
-        'first_name': 'Nikita',
-        'last_name': 'Driven'
-    })
-    # print(output_test)
+    env = Environment()
+    env.loader = FileSystemLoader(folder)
+    tmpl = env.get_template(template_name)
+    return tmpl.render(**kwargs)
